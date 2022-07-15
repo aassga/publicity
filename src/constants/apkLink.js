299a881dad8abdd3c3afbed85329bcab443077e0
bertha.l <cat303c@gmail.com>
2022-07-15 15:10:04 +0800

    20220715: 更換apk連結＆qrcode

--- src/constants/apkLink.js
+++ src/constants/apkLink.js
@@ -1,3 +1,3 @@
-const apkLink = 'https://apk.hichat.info/download/Hailiao_2022-06-30_v1.4.0-release.apk'
+const apkLink = 'https://apk.hichat.info/download/Hailiao_2022-07-14_v1.4.0-release.apk'
 
 export default apkLink
