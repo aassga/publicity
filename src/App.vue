3632235be337be886e8667748c62717ace4f0aa4
Michael.wu <98412060@stud.sju.edu.tw>
2022-12-21 14:15:08 +0800

    #2022.12.21 #app 邊界width 改為100%

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <style lang="scss">
 #app {
-  width: 100vw;
+  width: 100%;
   overflow: hidden;
   font-family: "Noto Sans", Roboto, -apple-system,BlinkMacSystemFont,"Segoe UI","Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
   -webkit-font-smoothing: antialiased;
